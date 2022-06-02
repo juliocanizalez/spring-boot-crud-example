974690e06c4d49bf7b8a9af39467b59459d00111
Elisa <elisa.pereira2000@gmail.com>
2022-06-01 22:27:56 -0600

    db script updated

--- EncuestaLab2JEC.sql
+++ EncuestaLab2JEC.sql
@@ -3,11 +3,12 @@ CREATE DATABASE EncuestaLab2JEC
 USE EncuestaLab2JEC;
 
 CREATE TABLE EncuestaJEC(
-id_encuesta INT NOT NULL PRIMARY KEY auto_increment,
+id INT NOT NULL PRIMARY KEY auto_increment,
 nombre VARCHAR(150) NOT NULL,
 sexo VARCHAR(9) NOT NULL,
 deporte_favorito VARCHAR(10) NOT NULL,
 nivel_estudio VARCHAR(10) NOT NULL,
-temas VARCHAR(10) NOT NULL
+temas VARCHAR(10) NOT NULL,
+fecha DATETIME NOT NULL
 );
 
